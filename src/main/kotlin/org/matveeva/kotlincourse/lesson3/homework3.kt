7248adb2e707d9f53789f05166d87691bc1ec71a
lizavetta67 <kozlovaev93@gmail.com>
2024-09-15 16:27:20 +0300

    Commit homework for lesson3,lesson4

--- src/main/kotlin/org/matveeva/kotlincourse/lesson3/homework3.kt
+++ src/main/kotlin/org/matveeva/kotlincourse/lesson3/homework3.kt
@@ -16,44 +16,62 @@ var count: Int = 0
         if (value >=1) field=value
     }
 // Домашнее задание
-// изменяемость, обязательность, возможность начальной установки,
-// ресурсоёмкие вычисления значения,
-// необходимость проверки устанавливаемого значения на валидность или
-// запрет доступа на установку при возможности чтения,
-// публичность и приватность данных.
-
 
 //Название мероприятия. Может быть изменено.
-var eventname: String = "Hackathon Survival"
+var eventName: String = "Hackathon Survival"
 //Дата проведения
-var dateevent: String = "2020-01-01 00:00:00"
+var dateEvent: String = "2020-01-01 00:00:00"
 //Место проведения
-var localevent: String = "г. Москва"
+var localEvent: String = "г. Москва"
 //Подробный бюджет мероприятия, включая расходы на оборудование, кейтеринг и другие
-val budget: String by lazy {
+private val budget: String by lazy {
     "Here will be budget"
 }
 //Количество участников
 var members: Int = 0
     get() = field
-    set(value) {
-        if (value >=1000) print("Места не хватит всем")
-        else field=value
-    }
+    private set(value) {field=value}
 //Длительность хакатона
-var duration: Int = 2
+var duration: Long = 2*60*60
 //Контактная информация и условия соглашений с поставщиками пищи, оборудования и других услуг.
-var contacts: String = "blabla@gmail.com Телефон для связи +7(999)888-77-55"
-lateinit var confirmInfo: String
+private lateinit var confirmInfo: String
 //Текущее состояние хакатона (статус)
 var status: String = "Start"
 //Список спонсоров
-var sponsorarray: Array<String> = arrayOf("Ростех", "Министерство образования", "ООО 'Инвестиции'")
+var sponsorArray: Array<String> = arrayOf("Ростех", "Министерство образования", "ООО 'Инвестиции'")
 //Бюджет мероприятия
-var budgetinfo: Double = 0.0
-//Текущий уровень доступа к интернету???
-
+var budgetInfo: Double = 0.0
+//Текущий уровень доступа к интернету
+var internetLevel: Int = 5
 //Информация о транспортировке оборудования, распределении ресурсов и координации между различными командами поддержки.
+private var infoTransport: String = ""
 //Количество команд
-var countteam: Double = 0.0
+var countTeam: Int = 0
 //Перечень задач
+val tasks: String = "list of Tasks"
+//Список доступного оборудования
+var listOfVariableEquipment: String = "50"
+//Список доступного оборудования
+var listOfFreeEquipment: String = "50"
+//План эвакуации
+val planEv: String = "Plan"
+//Текущая температура
+var temperatura: Float = 25.5f
+//Политика конфиденциальности
+val nda: String = " описание NDA"
+//Доступность медицинской помощи
+var doctorFlag: Boolean = true
+//Фидбек участников
+private val feedback: String by lazy {
+    "Понравилось ли мероприятие"
+}
+//Настроение
+lateinit var mood: String
+//Специальный гость
+lateinit var SuperStar: String
+//Подробный план хакатона
+val planWithDetail: String by lazy {
+    "Время начало и этапы хакатона"
+}
+
+
